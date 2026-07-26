b5c460b767963227d9aca49e78d1ab353ec59b19
harashkumar399-jpg <harashkumar399@gmail.com>
2026-07-27 01:10:27 +0530

    feat: configure MongoDB connection

---
+++ src/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose'); 
+
+function connectToDB() {
+
+    mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to DB');
+    })
+    .catch((err) => {
+        console.error('Error connecting to DB:', err);
+        process.exit(1);
+    });
+
+}
+
+module.exports = connectToDB;
