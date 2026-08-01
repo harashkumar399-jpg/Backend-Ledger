72f3547f2923ae3c61ea117c8c107256ab05cc70
harashkumar399-jpg <harashkumar399@gmail.com>
2026-08-01 23:42:03 +0530

    add routes for retrieve all accounts of logged-n user and to fetch balance of an account

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -14,4 +14,20 @@ const router = express.Router();
  */
 router.post('/', authMiddleware.authMiddleware, accountController.createAccountController);
 
+
+/**
+ * - GET /api/accounts
+ * - Get all accounts of the logged-in user
+ * - Protected Route
+ */
+router.get('/', authMiddleware.authMiddleware, accountController.getUserAccountsController);
+
+
+/**
+ * - GET /api/accounts/balance/:accountId
+ * - Get the balance of a specific account
+ * - Protected Route
+ */
+router.get('/balance/:accountId', authMiddleware.authMiddleware, accountController.getAccountBalanceController);
+
 module.exports = router;
