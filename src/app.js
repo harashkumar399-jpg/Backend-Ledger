e04f00e436ade079c0c9e46b9145aec6af10d65a
harashkumar399-jpg <harashkumar399@gmail.com>
2026-08-01 23:26:49 +0530

    Add routes for Authrization, Accounts and Transactions

--- src/app.js
+++ src/app.js
@@ -2,11 +2,17 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 
 const authRouter = require('./routes/auth.routes');
+const accountRouter = require('./routes/account.routes');
+const transactionRouter = require('./routes/transaction.routes');
+
 
 const app = express();
+exports.app = app;
 
 app.use(cookieParser());
 app.use(express.json());
 app.use('/api/auth', authRouter);
+app.use('/api/accounts', accountRouter);
+app.use('/api/transactions', transactionRouter);
 
 module.exports = app;
