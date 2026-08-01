e157b026ecb278157d9942199b8142c801ad4075
harashkumar399-jpg <harashkumar399@gmail.com>
2026-08-01 23:28:34 +0530

    Add transaction successfull or transaction failed email service

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -51,6 +51,24 @@ async function sendRegistrationEmail(userEmail, name) {
     await sendEmail(userEmail, subject, text, html);
 }
 
+async function sendTransactionEmail(userEmail, name, amount, toAccount){
+    const subject = 'Transaction Successful';
+    const text = `Hello ${name},\n\nYour transaction of ${amount} to account ${toAccount} was successful.\n\nBest regards,\nThe Backend Ledger Team`;
+    const html = `<p>Hello ${name},</p><p>Your transaction of ${amount} to account ${toAccount} was successful.</p><p>Best regards,<br>The Backend Ledger Team</p>`;
+
+    await sendEmail(userEmail, subject, text, html);
+}
+
+    async function sendTransactionFailedEmail(userEmail, name, amount, toAccount){
+    const subject = 'Transaction Failed';
+    const text = `Hello ${name},\n\nYour transaction of ${amount} to account ${toAccount} failed.\n\nBest regards,\nThe Backend Ledger Team`;
+    const html = `<p>Hello ${name},</p><p>Your transaction of ${amount} to account ${toAccount} failed.</p><p>Best regards,<br>The Backend Ledger Team</p>`;
+
+    await sendEmail(userEmail, subject, text, html);
+}
+
 module.exports = {
-    sendRegistrationEmail
+    sendRegistrationEmail,
+    sendTransactionEmail,
+    sendTransactionFailedEmail
 }
