d510d30e09103b6c51539047f1fa18c832517bfd
harashkumar399-jpg <harashkumar399@gmail.com>
2026-08-01 23:44:51 +0530

    add feature to check token is blacklisted or not, so that unauthorized access of account can be prevented

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,6 +1,6 @@
 const userModel = require('../models/user.model');
 const jwt = require('jsonwebtoken');
-
+const blacklistTokenModel = require('../models/blackList.model');
 
 
 
@@ -11,6 +11,14 @@ async function authMiddleware(req, res, next) {
     if(!token) {
         return res.status(401).json({
             message: "Unauthorize access, token is missing"
+        })
+    }
+    
+    const isBlacklisted = await blacklistTokenModel.findOne({ token });
+
+    if(isBlacklisted) {
+        return res.status(401).json({
+            message: "Unauthorized access, token is blacklisted"
         })
     }
 
@@ -32,6 +40,49 @@ async function authMiddleware(req, res, next) {
     }
 }
 
+async function authSystemUserMiddleware(req, res, next) {
+
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[ 1 ]
+
+    if(!token) {
+        return res.status(401).json({
+            message: "Unauthorize access, token is missing"
+        })
+    }
+
+    const isBlacklisted = await blacklistTokenModel.findOne({ token });
+
+    if(isBlacklisted) {
+        return res.status(401).json({
+            message: "Unauthorized access, token is blacklisted"
+        })
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        const user = await userModel.findById(decoded.userId).select("+systemUser");
+        
+         if(!user.systemUser) {
+            return res.status(403).json({
+                message: "Forbidden access, not a system user"
+            })
+         }
+         
+         req.user = user;
+
+         return next();
+
+    }catch(err){
+
+        return res.status(401).json({
+            message: "Unauthorized access, token is invalid"
+        })
+    }
+
+}
+
 module.exports = {
-    authMiddleware
+    authMiddleware,
+    authSystemUserMiddleware
 }
